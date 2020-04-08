e0f1768b9a03af0921ba054749c4d429cf091085
CiucIJS <gregaa.morano@gmail.com>
2020-04-08 09:41:08 +0200

    20min

--- examples/stats-app/stats-app.c
+++ examples/stats-app/stats-app.c
@@ -39,7 +39,7 @@
 
 /*---------------------------------------------------------------------------*/
 #define SECOND 		  (1000)
-#define MAX_APP_TIME  (SECOND * 600) 
+#define MAX_APP_TIME  (SECOND * 1200) 
 
 uint32_t counter = 0;
 
