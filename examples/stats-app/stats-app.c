a4ce4661706a1323db333af33e69a3e60b071052
Ciuc <gregaa.morano@gmail.com>
2020-05-12 12:08:10 +0200

    stats-app output commands

--- examples/stats-app/stats-app.c
+++ examples/stats-app/stats-app.c
@@ -39,13 +39,15 @@
 
 /*---------------------------------------------------------------------------*/
 #define SECOND 		  (1000)
-#define MAX_APP_TIME  (SECOND * 1200) 
+#define MAX_APP_TIME  (SECOND * 60 * 60) 
 
 uint32_t counter = 0;
 
+enum STATS_commands {cmd_start, cmd_stop, app_duration};
 /*---------------------------------------------------------------------------*/
 void STATS_print_help(void);
 void STATS_input_command(char *data);
+void STATS_output_command(uint8_t cmd);
 void STATS_set_device_as_root(void);
 void STATS_close_app(void);
 
@@ -68,68 +70,6 @@ PROCESS_THREAD(serial_input_process, ev, data)
     PROCESS_END();
 }
 
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(stats_process, ev, data)
-{
-	static struct etimer timer;
-
-	PROCESS_BEGIN();
-
-	printf(">Starting app! \n");
-	counter = 0;  
-
-	// Empty buffers if they have some values from before
-	RF2XX_STATS_RESET();
-	STATS_clear_packet_stats();
-	STATS_clear_background_noise();
-
-	printf("AD %d\n", (MAX_APP_TIME/1000));
-
-	// Optional: print help into log file
-	STATS_print_help();
-
-	etimer_set(&timer, 1);  //ms = 1, sec = 1000
-
-	while(1) {
-		counter++;
-
-	#if STATS_BGN_MEASURMENT_EVERY_10MS
-		if((counter%(10 * SECOND)) == 0){
-			STATS_print_background_noise();
-		}
-		else if((counter % 100) == 0) {
-			STATS_update_background_noise();
-		}
-	#else
-		if((counter % SECOND) == 0){
-			STATS_print_background_noise();
-		}
-		else{
-			STATS_update_background_noise();
-		}
-	#endif
-		// PROCESS_PAUSE();
-		// Every 10 seconds print packet statistics and clear the buffer
-		if((counter%(SECOND * 10)) == 0){
-			STATS_print_packet_stats();
-		}
-
-		// After max time send stop command ('=') and print driver statistics
-		if(counter == (MAX_APP_TIME)){
-			STATS_close_app();
-			PROCESS_EXIT();
-		}
-
-		// Wait for the periodic timer to expire and then restart the timer.
-		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-		etimer_reset(&timer);
-	}
-
-	PROCESS_END();
-}
-
-
-/*---------------------------------------------------------------------------*/
 void
 STATS_input_command(char *data){
     char cmd = data[0];
@@ -152,6 +92,76 @@ STATS_input_command(char *data){
     }
 }
 
+void
+STATS_output_command(uint8_t cmd)
+{
+	switch(cmd){
+		case cmd_start:
+			printf("> \n");
+			break;
+
+		case cmd_stop:
+			printf("= \n");
+			break;
+
+		case app_duration:
+			printf("AD %d\n", (MAX_APP_TIME/1000));
+			break;
+		
+		default:
+			printf("Unknown output command \n");
+			break;
+	}
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(stats_process, ev, data)
+{
+	static struct etimer timer;
+
+	PROCESS_BEGIN();
+
+	// Respond to LGTC
+	STATS_output_command(cmd_start);
+	counter = 0;  
+
+	// Empty buffers if they have some values from before
+	RF2XX_STATS_RESET();
+	STATS_clear_packet_stats();
+	STATS_clear_background_noise();
+
+	STATS_output_command(app_duration);
+
+	// Optional: print help into log file
+	STATS_print_help();
+
+	etimer_set(&timer, 1);  //ms = 1, sec = 1000
+
+	while(1) {
+		counter++;
+
+		if((counter%(10 * SECOND)) == 0){
+			STATS_print_background_noise();
+			STATS_print_packet_stats();
+		}
+		else if((counter % 10) == 0) {
+			STATS_update_background_noise();
+		}
+
+		// After max time send stop command ('=') and print driver statistics
+		if(counter == (MAX_APP_TIME)){
+			STATS_close_app();
+			PROCESS_EXIT();
+		}
+
+		// Wait for the periodic timer to expire and then restart the timer.
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+		etimer_reset(&timer);
+	}
+
+	PROCESS_END();
+}
+
 /*---------------------------------------------------------------------------*/
 void
 STATS_set_device_as_root(void){
@@ -172,8 +182,9 @@ void
 STATS_close_app(void){
 
 	STATS_print_driver_stats();
+	
 	// Send '=' cmd to stop the monitor
-	printf("=End monitoring serial port\n");
+	STATS_output_command(cmd_stop);
 
 	// Empty buffers
 	RF2XX_STATS_RESET();
