9029848c910146e34616db63e29c98b543fe8aff
CiucIJS <gregaa.morano@gmail.com>
2020-07-17 10:00:49 +0200

    fff drawed back, now testing mc on testbed | stats-app-gregor renamed to stats-app-csma

--- examples/stats-app/stats-app-root.c
+++ examples/stats-app/stats-app-root.c
@@ -2,24 +2,26 @@
  * STATS-APP 
  * -----------------------------------------------------------------------------
 */
+#include <stdio.h>
+#include "dev/serial-line.h"
+#include <string.h>
+#include <inttypes.h>
 
 #include "contiki.h"
 #include "contiki-lib.h"
 #include "contiki-net.h"
 
-#include <stdio.h>
-#include <string.h>
-#include "dev/serial-line.h"
-
-#include "arch/platform/vesna/dev/at86rf2xx/rf2xx.h"
-#include "arch/platform/vesna/dev/at86rf2xx/rf2xx_stats.h"
-
 #include "net/ipv6/uip.h"
 #include "net/routing/rpl-classic/rpl-private.h"
 #include "net/ipv6/multicast/uip-mcast6.h"
 
+#include "arch/platform/vesna/dev/at86rf2xx/rf2xx.h"
+#include "arch/platform/vesna/dev/at86rf2xx/rf2xx_stats.h"
+
 #define DEBUG DEBUG_PRINT
 #include "net/ipv6/uip-debug.h"
+#include "net/routing/routing.h"
+
 
 #if !NETSTACK_CONF_WITH_IPV6 || !UIP_CONF_ROUTER || !UIP_IPV6_MULTICAST || !UIP_CONF_IPV6_RPL
 #error "This example can not work with the current contiki configuration"
@@ -31,12 +33,17 @@
 #define BGN_MEASURE_TIME_MS (10)
 #define MAX_APP_TIME  (60 * 60 * 2) //12 hours
 
+#define MAX_PAYLOAD_LEN 120
 #define MCAST_SINK_UDP_PORT 3001 /* Host byte order */
+#define SEND_INTERVAL CLOCK_SECOND /* clock ticks */
+#define ITERATIONS 1000 /* messages */
+#define START_DELAY 60 //Start sending messages START_DELAY secs after we start
 
 uint32_t counter = 0;
 
-static struct uip_udp_conn *sink_conn;
-static uint32_t msg_count;
+static struct uip_udp_conn * mcast_conn;
+static char buf[MAX_PAYLOAD_LEN];
+static uint32_t seq_id = 150;
 
 enum STATS_commands {cmd_start, cmd_stop, app_duration};
 /*---------------------------------------------------------------------------*/
@@ -46,13 +53,12 @@ void STATS_output_command(uint8_t cmd);
 void STATS_set_device_as_root(void);
 void STATS_close_app(void);
 
-static uip_ds6_maddr_t * STATS_join_mcast_group(void);
-
+void STATS_prepare_mcast(void);
+void STATS_multicast_send(void);
 /*---------------------------------------------------------------------------*/
 PROCESS(stats_process, "Stats app process");
 PROCESS(serial_input_process, "Serial input command");
 PROCESS(bgn_process, "Background noise process");
-PROCESS(mcast_sink_process, "Multicast Sink");
 
 AUTOSTART_PROCESSES(&serial_input_process);
 
@@ -73,7 +79,7 @@ STATS_input_command(char *data){
     char cmd = data[0];
     switch(cmd){
       case '>':
-		    process_start(&bgn_process, NULL);
+		//process_start(&bgn_process, NULL);
         process_start(&stats_process, NULL);
         break;
       
@@ -153,10 +159,13 @@ PROCESS_THREAD(stats_process, ev, data)
 
 	STATS_print_help();
 
-	process_start(&mcast_sink_process, NULL);
+	// Set device as root
+	STATS_set_device_as_root();
 
 	// Prepare multicast engine
-	
+	printf("Multicast engine: '%s' \n",UIP_MCAST6.name);
+	STATS_prepare_mcast();
+
 	etimer_set(&timer, SECOND);
 
 	while(1) {
@@ -164,7 +173,12 @@ PROCESS_THREAD(stats_process, ev, data)
 
 		if((counter % 10) == 0){
 			STATS_print_packet_stats();
-			STATS_print_background_noise();
+			//STATS_print_background_noise();
+		}
+
+		// After one minute, send MC message every second
+		if(counter > START_DELAY){
+			STATS_multicast_send();
 		}
 		
 		// After max time send stop command ('=') and print driver statistics
@@ -249,67 +263,47 @@ STATS_print_help(void){
 }
 
 
-
 /*---------------------------------------------------------------------------*/
-static uip_ds6_maddr_t *
-STATS_join_mcast_group(void)
+void
+STATS_multicast_send(void)
 {
-  uip_ipaddr_t addr;
-  uip_ds6_maddr_t *rv;
-  const uip_ipaddr_t *default_prefix = uip_ds6_default_prefix();
+  uint32_t id;
 
-  /* First, set our v6 global */
-  uip_ip6addr_copy(&addr, default_prefix);
-  uip_ds6_set_addr_iid(&addr, &uip_lladdr);
-  uip_ds6_addr_add(&addr, 0, ADDR_AUTOCONF);
+  id = uip_htonl(seq_id);
+  memset(buf, 0, MAX_PAYLOAD_LEN);
+  memcpy(buf, &id, sizeof(seq_id));
 
+  PRINTF("Send to: ");
+  PRINT6ADDR(&mcast_conn->ripaddr);
+  PRINTF(" Remote Port %u,", uip_ntohs(mcast_conn->rport));
+  PRINTF(" (msg=0x%08"PRIx32")", uip_ntohl(*((uint32_t *)buf)));
+  PRINTF(" %lu bytes\n", (unsigned long)sizeof(id));
+
+  seq_id++;
+
+  if(seq_id == 200){
+    seq_id = 0;
+  }
+  uip_udp_packet_send(mcast_conn, buf, sizeof(id));
+}
+
+void
+STATS_prepare_mcast(void)
+{
+  uip_ipaddr_t ipaddr;
+
+#if UIP_MCAST6_CONF_ENGINE == UIP_MCAST6_ENGINE_MPL
+/*
+ * MPL defines a well-known MPL domain, MPL_ALL_FORWARDERS, which
+ *  MPL nodes are automatically members of. Send to that domain.
+ */
+  uip_ip6addr(&ipaddr, 0xFF03,0,0,0,0,0,0,0xFC);
+#else
   /*
    * IPHC will use stateless multicast compression for this destination
    * (M=1, DAC=0), with 32 inline bits (1E 89 AB CD)
    */
-  uip_ip6addr(&addr, 0xFF1E,0,0,0,0,0,0x89,0xABCD);
-  rv = uip_ds6_maddr_add(&addr);
-
-  if(rv) {
-    PRINTF("Joined multicast group ");
-    PRINT6ADDR(&uip_ds6_maddr_lookup(&addr)->ipaddr);
-    PRINTF("\n");
-  }
-  return rv;
-}
-
-PROCESS_THREAD(mcast_sink_process, ev, data)
-{
-  PROCESS_BEGIN();
-
-  	printf("Multicast engine: '%s' \n",UIP_MCAST6.name);
-
-	#if UIP_MCAST6_CONF_ENGINE != UIP_MCAST6_ENGINE_MPL
-		if(STATS_join_mcast_group() == NULL) {
-			PRINTF("Failed to join multicast group\n");
-			//PROCESS_EXIT();
-		}
-	#endif
-	
-	msg_count = 0;
-	sink_conn = udp_new(NULL, UIP_HTONS(0), NULL);
-  	udp_bind(sink_conn, UIP_HTONS(MCAST_SINK_UDP_PORT));
-
-	PRINTF("Listening: ");
-  	PRINT6ADDR(&sink_conn->ripaddr);
- 	PRINTF(" local/remote port %u/%u\n",UIP_HTONS(sink_conn->lport), UIP_HTONS(sink_conn->rport));
-
-  while(1) {
-    PROCESS_YIELD();
-    if(ev == tcpip_event) {
-      
-	  if(uip_newdata()) {
-    		msg_count++;
-    		PRINTF("In: [0x%08lx], TTL %u, total %u\n",
-        		(unsigned long)uip_ntohl((unsigned long) *((uint32_t *)(uip_appdata))),
-        		UIP_IP_BUF->ttl, msg_count);
-  		}
-    }
-  }
-  PROCESS_END();
+  uip_ip6addr(&ipaddr, 0xFF1E,0,0,0,0,0,0x89,0xABCD);
+#endif
+  mcast_conn = udp_new(&ipaddr, UIP_HTONS(MCAST_SINK_UDP_PORT), NULL);
 }
