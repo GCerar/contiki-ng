9a21b98d303a452847e2fe4574d9a845dd1e732d
CiucIJS <gregaa.morano@gmail.com>
2020-07-20 13:08:53 +0200

    sad

--- examples/stats-app/project-conf.h
+++ examples/stats-app/project-conf.h
@@ -5,10 +5,10 @@
 // All logs to LOG_LEVEL_NONE
 #define LOG_CONF_LEVEL_MAIN                        LOG_LEVEL_INFO
 #define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_NONE
-#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_NONE
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_NONE
 #define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_NONE
-#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_NONE
+#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_NONE
 #define LOG_CONF_LEVEL_RF2XX                       LOG_LEVEL_WARN
 #define TSCH_LOG_CONF_PER_SLOT                     (0)
@@ -23,7 +23,7 @@
 #define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE          (uint8_t[]){ 15 }
 //#define IEEE802154_CONF_DEFAULT_CHANNEL             (26)
 
-// PAN-ID for FFF experiment
+// PAN-ID for FFF experiment - to join the same network
 #define IEEE802154_CONF_PANID                       (0xBEEF)
 
 // Testbed can have max 21 devices
